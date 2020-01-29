1d282908b72bc07f7851e759446859c9a0852d13
rksmart1110 <60419074+rksmart1110@users.noreply.github.com>
2020-01-29 14:23:31 +0530

    loop_or_not.py
    this python program will generate a loop if user did not enter 'q' as a input

---
+++ loop_or_not.py
@@ -0,0 +1,34 @@
+a=input("enter a string\t")
+if(a=='q'):
+    print('end')
+else:
+    while (True):
+        print("looping")
+        
+'''
+sample input & output
+
+enter a string a
+looping
+looping
+looping
+looping
+looping
+looping
+looping
+looping
+looping
+looping
+looping
+.
+.
+.
+(loop continues)
+
+sample input & output 2
+
+enter a string q
+end
+>>> 
+
+'''
